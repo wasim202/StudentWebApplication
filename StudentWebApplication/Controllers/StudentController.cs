c7f0589336351540d237bbf1d8cf0683d4550907
Wasim.Mohammad <Wasim.Mohammad@itecj.com>
2025-09-03 10:53:33 +0300

    Add Student Model & Controller

---
+++ StudentWebApplication/Controllers/StudentController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using StudentWebApplication.Models;
+
+namespace StudentWebApplication.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly IHttpClientFactory _clientFactory;
+        private readonly IConfiguration _config;
+
+        public StudentController(IHttpClientFactory clientFactory, IConfiguration config)
+        {
+            _clientFactory = clientFactory;
+            _config = config;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var token = HttpContext.Session.GetString("JWToken");
+            if (string.IsNullOrEmpty(token))
+                return RedirectToAction("Login", "Account");
+
+            var client = _clientFactory.CreateClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var response = await client.GetAsync(_config["ApiSettings:BaseUrl"] + "/api/student");
+
+            if (!response.IsSuccessStatusCode)
+                return Unauthorized();
+
+            var result = await response.Content.ReadAsStringAsync();
+            var students = JsonConvert.DeserializeObject<List<Student>>(result);
+
+            return View(students);
+        }
+    }
+}
---
+++ StudentWebApplication/Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace StudentWebApplication.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public int Age { get; set; }
+    }
+}
