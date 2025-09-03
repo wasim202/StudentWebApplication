4904447af4dd48b85766f2e81213c1f67c9f36c3
Wasim.Mohammad <Wasim.Mohammad@itecj.com>
2025-09-03 09:31:29 +0300

    Add Login View Model

---
+++ StudentWebApplication/Models/LoginVM.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentWebApplication.Models
+{
+    public class LoginVM
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
