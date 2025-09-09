ecfc1814cb5966518174e4c8545eb3b6b7e308d0
Wasim.Mohammad <Wasim.Mohammad@itecj.com>
2025-09-09 10:52:47 +0300

    Add IsAdmin field to the Model

---
+++ StudentWebApplication/Models/UserLogin.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentWebApplication.Models
+{
+    public class UserLogin
+    {
+        [Key]
+        public int Id { get; set; } 
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+        public Boolean Isadmin { get; set; } 
+    }
+}
