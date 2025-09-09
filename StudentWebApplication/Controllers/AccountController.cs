c865fcd8bf7d6972f68a87ed0a4cd7b7e5d7b134
Wasim.Mohammad <Wasim.Mohammad@itecj.com>
2025-09-09 13:58:08 +0300

    Add Register Action to AccountController

--- StudentWebApplication/Controllers/AccountController.cs
+++ StudentWebApplication/Controllers/AccountController.cs
@@ -1,74 +1,4 @@
-﻿//using Microsoft.AspNetCore.Mvc;
-//using Newtonsoft.Json;
-//using StudentWebApplication.Models;
-//using System.IdentityModel.Tokens.Jwt;
-//using System.Text;
-
-//namespace StudentWebApplication.Controllers
-//{
-//    public class AccountController : Controller
-//    {
-//        private readonly IHttpClientFactory _clientFactory;
-//        private readonly IConfiguration _config;
-
-//        public AccountController(IHttpClientFactory clientFactory, IConfiguration config)
-//        {
-//            _clientFactory = clientFactory;
-//            _config = config;
-//        }
-//        [HttpGet]
-//        public IActionResult login()
-//        {
-//            return View();
-//        }
-
-//        [HttpPost]
-//        public async Task<IActionResult> Login(UserLogin model)
-//        {
-//            if (!ModelState.IsValid) return View(model);
-
-//            var client = _clientFactory.CreateClient();
-//            var apiUrl = _config["ApiSettings:BaseUrl"] + "/api/auth/login";
-
-//            var json = JsonConvert.SerializeObject(model);
-//            var content = new StringContent(json, Encoding.UTF8, "application/json");
-
-//            var response = await client.PostAsync(apiUrl, content);
-
-//            if (!response.IsSuccessStatusCode)
-//            {
-//                ModelState.AddModelError("", "Invalid login attempt");
-//                return View(model);
-//            }
-
-//            var result = await response.Content.ReadAsStringAsync();
-//            var tokenObj = JsonConvert.DeserializeObject<Dictionary<string, string>>(result);
-
-//            HttpContext.Session.SetString("JWToken", tokenObj["token"]);
-
-
-
-//            return RedirectToAction("Index", "Student");
-//        }
-
-//        public static class JwtHelper
-//        {
-//            public static bool IsAdmin(string token)
-//            {
-//                var handler = new JwtSecurityTokenHandler();
-//                var jwtToken = handler.ReadJwtToken(token);
-
-//                var isAdminClaim = jwtToken.Claims.FirstOrDefault(c => c.Type == "IsAdmin");
-//                return isAdminClaim != null && bool.Parse(isAdminClaim.Value);
-//            }
-//        }
-
-
-//    }
-//}
-
-
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using StudentWebApplication.Models;
 using System.IdentityModel.Tokens.Jwt;
@@ -86,6 +16,37 @@ namespace StudentWebApplication.Controllers
             _clientFactory = clientFactory;
             _config = config;
         }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(UserRegister model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            var client = _clientFactory.CreateClient();
+            var apiUrl = _config["ApiSettings:BaseUrl"] + "/api/auth/register";
+
+            var json = JsonConvert.SerializeObject(model);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await client.PostAsync(apiUrl, content);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                ModelState.AddModelError("", "Registration failed");
+                return View(model);
+            }
+
+            // Redirect to login page after successful signup
+            return RedirectToAction("Login");
+        }
+
 
         [HttpGet]
         public IActionResult Login()
