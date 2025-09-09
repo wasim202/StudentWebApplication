1a71497571f007de1899464450a1a06d45c8b1ff
Wasim.Mohammad <Wasim.Mohammad@itecj.com>
2025-09-09 10:49:13 +0300

    Add JWT Helper for IsAdmin login

---
+++ StudentWebApplication/Helpers/JwtHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+
+namespace StudentWebApplication.Helpers
+{
+    public class JwtHelper
+    {
+        public static bool IsAdmin(string token)
+        {
+            if (string.IsNullOrEmpty(token)) return false;
+
+            var handler = new JwtSecurityTokenHandler();
+            var jwtToken = handler.ReadJwtToken(token);
+
+            var isAdminClaim = jwtToken.Claims.FirstOrDefault(c => c.Type == "IsAdmin");
+            return isAdminClaim != null && bool.TryParse(isAdminClaim.Value, out bool isAdmin) && isAdmin;
+        }
+    }
+
+}
+
