7d668729b48020292e55bc533ea4a67b5be54e36
Wasim.Mohammad <Wasim.Mohammad@itecj.com>
2025-09-09 11:11:01 +0300

    Add a token for every endpoint

--- StudentWebApplication/Controllers/StudentController.cs
+++ StudentWebApplication/Controllers/StudentController.cs
@@ -1,39 +1,112 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
-using System.Net.Http.Headers;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using StudentWebApplication.Helpers;
 using StudentWebApplication.Models;
+using StudentWebApplication.Services;
 
 namespace StudentWebApplication.Controllers
 {
     public class StudentController : Controller
     {
-        private readonly IHttpClientFactory _clientFactory;
-        private readonly IConfiguration _config;
+        private readonly StudentService _service;
 
-        public StudentController(IHttpClientFactory clientFactory, IConfiguration config)
+        public StudentController(StudentService service)
         {
-            _clientFactory = clientFactory;
-            _config = config;
+            _service = service;
         }
 
+        [HttpGet]
+        //[Authorize(Roles = "Admin,Viewer")]
         public async Task<IActionResult> Index()
         {
+            var students = await _service.GetAllAsync();
             var token = HttpContext.Session.GetString("JWToken");
-            if (string.IsNullOrEmpty(token))
-                return RedirectToAction("Login", "Account");
 
-            var client = _clientFactory.CreateClient();
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            if (JwtHelper.IsAdmin(token))
+            {
+                return View("IndexAdmin", students);
+            }
+            else
+            {
+                return View("IndexViewer", students);
+            }
+            //return View(students);
+        }
 
-            var response = await client.GetAsync(_config["ApiSettings:BaseUrl"] + "/api/student");
+        [HttpGet]
+        public IActionResult Create()
+        {
+            var token = HttpContext.Session.GetString("JWToken");
+            if (!JwtHelper.IsAdmin(token))
+                return RedirectToAction("Index");
 
-            if (!response.IsSuccessStatusCode)
-                return Unauthorized();
+            return View();
+        }
 
-            var result = await response.Content.ReadAsStringAsync();
-            var students = JsonConvert.DeserializeObject<List<Student>>(result);
+        [HttpPost]
+        //[Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Create(Student student)
+        {
+            var token = HttpContext.Session.GetString("JWToken");
+            if (!JwtHelper.IsAdmin(token))
+                return RedirectToAction("Index");
 
-            return View(students);
+            await _service.AddAsync(student);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var token = HttpContext.Session.GetString("JWToken");
+            if (!JwtHelper.IsAdmin(token))
+                return RedirectToAction("Index");
+
+            var student = await _service.GetByIdAsync(id);
+            return View(student);
+        }
+
+        [HttpPost]
+        //[Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Edit(Student student)
+        {
+            var token = HttpContext.Session.GetString("JWToken");
+            if (!JwtHelper.IsAdmin(token))
+                return RedirectToAction("Index");
+
+
+            await _service.UpdateAsync(student);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var token = HttpContext.Session.GetString("JWToken");
+            if (!JwtHelper.IsAdmin(token))
+                return RedirectToAction("Index");
+
+            var student = await _service.GetByIdAsync(id);
+            return View(student);
+        }
+
+        
+        [HttpPost]
+        //[Authorize(Roles = "Admin")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var token = HttpContext.Session.GetString("JWToken");
+            if (!JwtHelper.IsAdmin(token))
+                return RedirectToAction("Index");
+
+
+            await _service.DeleteAsync(id);
+            return RedirectToAction("Index");
         }
     }
 }
+
+
+
+
+
