327ec56a98791177846be5a478c505e1b40fe763
Wasim.Mohammad <Wasim.Mohammad@itecj.com>
2025-09-09 11:07:28 +0300

    Change the User Model View to UserLogin Model Add ViewBag .Errorfor Invalid UserName or Password

--- StudentWebApplication/Controllers/AccountController.cs
+++ StudentWebApplication/Controllers/AccountController.cs
@@ -1,6 +1,77 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿//using Microsoft.AspNetCore.Mvc;
+//using Newtonsoft.Json;
+//using StudentWebApplication.Models;
+//using System.IdentityModel.Tokens.Jwt;
+//using System.Text;
+
+//namespace StudentWebApplication.Controllers
+//{
+//    public class AccountController : Controller
+//    {
+//        private readonly IHttpClientFactory _clientFactory;
+//        private readonly IConfiguration _config;
+
+//        public AccountController(IHttpClientFactory clientFactory, IConfiguration config)
+//        {
+//            _clientFactory = clientFactory;
+//            _config = config;
+//        }
+//        [HttpGet]
+//        public IActionResult login()
+//        {
+//            return View();
+//        }
+
+//        [HttpPost]
+//        public async Task<IActionResult> Login(UserLogin model)
+//        {
+//            if (!ModelState.IsValid) return View(model);
+
+//            var client = _clientFactory.CreateClient();
+//            var apiUrl = _config["ApiSettings:BaseUrl"] + "/api/auth/login";
+
+//            var json = JsonConvert.SerializeObject(model);
+//            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+//            var response = await client.PostAsync(apiUrl, content);
+
+//            if (!response.IsSuccessStatusCode)
+//            {
+//                ModelState.AddModelError("", "Invalid login attempt");
+//                return View(model);
+//            }
+
+//            var result = await response.Content.ReadAsStringAsync();
+//            var tokenObj = JsonConvert.DeserializeObject<Dictionary<string, string>>(result);
+
+//            HttpContext.Session.SetString("JWToken", tokenObj["token"]);
+
+
+
+//            return RedirectToAction("Index", "Student");
+//        }
+
+//        public static class JwtHelper
+//        {
+//            public static bool IsAdmin(string token)
+//            {
+//                var handler = new JwtSecurityTokenHandler();
+//                var jwtToken = handler.ReadJwtToken(token);
+
+//                var isAdminClaim = jwtToken.Claims.FirstOrDefault(c => c.Type == "IsAdmin");
+//                return isAdminClaim != null && bool.Parse(isAdminClaim.Value);
+//            }
+//        }
+
+
+//    }
+//}
+
+
+using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using StudentWebApplication.Models;
+using System.IdentityModel.Tokens.Jwt;
 using System.Text;
 
 namespace StudentWebApplication.Controllers
@@ -15,14 +86,15 @@ namespace StudentWebApplication.Controllers
             _clientFactory = clientFactory;
             _config = config;
         }
+
         [HttpGet]
-        public IActionResult login()
+        public IActionResult Login()
         {
             return View();
         }
 
         [HttpPost]
-        public async Task<IActionResult> Login(LoginVM model)
+        public async Task<IActionResult> Login(UserLogin model)
         {
             if (!ModelState.IsValid) return View(model);
 
@@ -36,7 +108,9 @@ namespace StudentWebApplication.Controllers
 
             if (!response.IsSuccessStatusCode)
             {
-                ModelState.AddModelError("", "Invalid login attempt");
+                //ModelState.AddModelError("", "Invalid login attempt");
+                // return View(model);
+                ViewBag.Error = "Invalid username or password.";
                 return View(model);
             }
 
